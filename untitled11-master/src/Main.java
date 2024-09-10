255a0b1a4d42c4d2ff6552d904fe088487a2fe18
Aliona <alionkakhuda28@gmail.com>
2024-09-10 15:17:36 +0300

    add_repo

---
+++ untitled11-master/src/Main.java
@@ -0,0 +1,13 @@
+
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        HumanIMB humanIMB = new HumanIMB(80, 1.52);
+        System.out.println(humanIMB.getResult());
+    }
+}
+
+
